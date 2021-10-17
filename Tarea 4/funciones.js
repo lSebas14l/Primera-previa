1c8ec02c751a7373bdc4c8bb317e332baa8c7376
lSebas14l <78990462+lSebas14l@users.noreply.github.com>
2021-10-16 20:13:19 -0500

    Add files via upload

---
+++ Tarea 4/funciones.js
@@ -0,0 +1,94 @@
+var numero; //Se crea la variable "numero" para guardas los resultados 
+//=================================================================================================================================
+// 1
+document.write("El número binario que resolveremos es: <br> 0000 "); //Document.write nos permite mostrar en pantalla el texto que pongamos entre los parenteisis "()" 
+numero = 0* Math.pow(2, 3) + 0* Math.pow(2, 2) + 0* Math.pow(2, 1) + 0* Math.pow(2, 0);   // le damos un valor a la variable numero con la operación correspondiente - Math.pow es una función que eleva a una potencia
+document.write("<br>El resultado es: "+numero +"<br><br>"); //Se muestra el resultado concatenando texto con la variable a mostrar
+document.write("<hr>"); // "hr" Es una etiquieta de html para crear una raya que separe las operaciones
+//=================================================================================================================================
+// 2
+document.write("El número binario que resolveremos es: <br> 0001 "); //Document.write nos permite mostrar en pantalla el texto que pongamos entre los parenteisis "()" 
+numero = 0* Math.pow(2, 3) + 0* Math.pow(2, 2) + 0* Math.pow(2, 1) + 1* Math.pow(2, 0);   // le damos un valor a la variable numero con la operación correspondiente - Math.pow es una función que eleva a una potencia
+document.write("<br>El resultado es: "+numero +"<br><br>"); //Se muestra el resultado concatenando texto con la variable a mostrar
+document.write("<hr>"); // "hr" Es una etiquieta de html para crear una raya que separe las operaciones
+//=================================================================================================================================
+// 3
+document.write("El número binario que resolveremos es: <br> 0010 "); //Document.write nos permite mostrar en pantalla el texto que pongamos entre los parenteisis "()" 
+numero = 0* Math.pow(2, 3) + 0* Math.pow(2, 2) + 1* Math.pow(2, 1) + 0* Math.pow(2, 0);   // le damos un valor a la variable numero con la operación correspondiente - Math.pow es una función que eleva a una potencia
+document.write("<br>El resultado es: "+numero +"<br><br>"); //Se muestra el resultado concatenando texto con la variable a mostrar
+document.write("<hr>"); // "hr" Es una etiquieta de html para crear una raya que separe las operaciones
+//=================================================================================================================================
+// 4
+document.write("El número binario que resolveremos es: <br> 0011 "); //Document.write nos permite mostrar en pantalla el texto que pongamos entre los parenteisis "()" 
+numero = 0* Math.pow(2, 3) + 0* Math.pow(2, 2) + 1* Math.pow(2, 1) + 1* Math.pow(2, 0);   // le damos un valor a la variable numero con la operación correspondiente - Math.pow es una función que eleva a una potencia
+document.write("<br>El resultado es: "+numero +"<br><br>"); //Se muestra el resultado concatenando texto con la variable a mostrar
+document.write("<hr>"); // "hr" Es una etiquieta de html para crear una raya que separe las operaciones
+//=================================================================================================================================
+// 5
+document.write("El número binario que resolveremos es: <br> 0100 "); //Document.write nos permite mostrar en pantalla el texto que pongamos entre los parenteisis "()" 
+numero = 0* Math.pow(2, 3) + 1* Math.pow(2, 2) + 0* Math.pow(2, 1) + 0* Math.pow(2, 0);   // le damos un valor a la variable numero con la operación correspondiente - Math.pow es una función que eleva a una potencia
+document.write("<br>El resultado es: "+numero +"<br><br>"); //Se muestra el resultado concatenando texto con la variable a mostrar
+document.write("<hr>"); // "hr" Es una etiquieta de html para crear una raya que separe las operaciones
+//=================================================================================================================================
+document.write("El número binario que resolveremos es: <br> 0101 "); //Document.write nos permite mostrar en pantalla el texto que pongamos entre los parenteisis "()" 
+numero = 0* Math.pow(2, 3) + 1* Math.pow(2, 2) + 0* Math.pow(2, 1) + 1* Math.pow(2, 0);   // le damos un valor a la variable numero con la operación correspondiente - Math.pow es una función que eleva a una potencia
+document.write("<br>El resultado es: "+numero +"<br><br>"); //Se muestra el resultado concatenando texto con la variable a mostrar
+document.write("<hr>"); // "hr" Es una etiquieta de html para crear una raya que separe las operaciones
+//=================================================================================================================================
+document.write("El número binario que resolveremos es: <br> 0110 "); //Document.write nos permite mostrar en pantalla el texto que pongamos entre los parenteisis "()" 
+numero = 0* Math.pow(2, 3) + 1* Math.pow(2, 2) + 1* Math.pow(2, 1) + 0* Math.pow(2, 0);   // le damos un valor a la variable numero con la operación correspondiente - Math.pow es una función que eleva a una potencia
+document.write("<br>El resultado es: "+numero +"<br><br>"); //Se muestra el resultado concatenando texto con la variable a mostrar
+document.write("<hr>"); // "hr" Es una etiquieta de html para crear una raya que separe las operaciones
+//=================================================================================================================================
+document.write("El número binario que resolveremos es: <br> 0111 "); //Document.write nos permite mostrar en pantalla el texto que pongamos entre los parenteisis "()" 
+numero = 0* Math.pow(2, 3) + 1* Math.pow(2, 2) + 1* Math.pow(2, 1) + 1* Math.pow(2, 0);   // le damos un valor a la variable numero con la operación correspondiente - Math.pow es una función que eleva a una potencia
+document.write("<br>El resultado es: "+numero +"<br><br>"); //Se muestra el resultado concatenando texto con la variable a mostrar
+document.write("<hr>"); // "hr" Es una etiquieta de html para crear una raya que separe las operaciones
+//=================================================================================================================================
+document.write("El número binario que resolveremos es: <br> 1000 "); //Document.write nos permite mostrar en pantalla el texto que pongamos entre los parenteisis "()" 
+numero = 1* Math.pow(2, 3) + 0* Math.pow(2, 2) + 0* Math.pow(2, 1) + 0* Math.pow(2, 0);   // le damos un valor a la variable numero con la operación correspondiente - Math.pow es una función que eleva a una potencia
+document.write("<br>El resultado es: "+numero +"<br><br>"); //Se muestra el resultado concatenando texto con la variable a mostrar
+document.write("<hr>"); // "hr" Es una etiquieta de html para crear una raya que separe las operaciones
+//=================================================================================================================================
+document.write("El número binario que resolveremos es: <br> 1001 "); //Document.write nos permite mostrar en pantalla el texto que pongamos entre los parenteisis "()" 
+numero = 1* Math.pow(2, 3) + 0* Math.pow(2, 2) + 0* Math.pow(2, 1) + 1* Math.pow(2, 0);   // le damos un valor a la variable numero con la operación correspondiente - Math.pow es una función que eleva a una potencia
+document.write("<br>El resultado es: "+numero +"<br><br>"); //Se muestra el resultado concatenando texto con la variable a mostrar
+document.write("<hr>"); // "hr" Es una etiquieta de html para crear una raya que separe las operaciones
+//=================================================================================================================================
+document.write("El número binario que resolveremos es: <br> 1010 "); //Document.write nos permite mostrar en pantalla el texto que pongamos entre los parenteisis "()" 
+numero = 1* Math.pow(2, 3) + 0* Math.pow(2, 2) + 1* Math.pow(2, 1) + 0* Math.pow(2, 0);   // le damos un valor a la variable numero con la operación correspondiente - Math.pow es una función que eleva a una potencia
+document.write("<br>El resultado es: "+numero +"<br><br>"); //Se muestra el resultado concatenando texto con la variable a mostrar
+document.write("<hr>"); // "hr" Es una etiquieta de html para crear una raya que separe las operaciones
+//=================================================================================================================================
+document.write("El número binario que resolveremos es: <br> 1011 "); //Document.write nos permite mostrar en pantalla el texto que pongamos entre los parenteisis "()" 
+numero = 1* Math.pow(2, 3) + 0* Math.pow(2, 2) + 1* Math.pow(2, 1) + 1* Math.pow(2, 0);   // le damos un valor a la variable numero con la operación correspondiente - Math.pow es una función que eleva a una potencia
+document.write("<br>El resultado es: "+numero +"<br><br>"); //Se muestra el resultado concatenando texto con la variable a mostrar
+document.write("<hr>"); // "hr" Es una etiquieta de html para crear una raya que separe las operaciones
+//=================================================================================================================================
+document.write("El número binario que resolveremos es: <br> 1100 "); //Document.write nos permite mostrar en pantalla el texto que pongamos entre los parenteisis "()" 
+numero = 1* Math.pow(2, 3) + 1* Math.pow(2, 2) + 0* Math.pow(2, 1) + 0* Math.pow(2, 0);   // le damos un valor a la variable numero con la operación correspondiente - Math.pow es una función que eleva a una potencia
+document.write("<br>El resultado es: "+numero +"<br><br>"); //Se muestra el resultado concatenando texto con la variable a mostrar
+document.write("<hr>"); // "hr" Es una etiquieta de html para crear una raya que separe las operaciones
+//=================================================================================================================================
+document.write("El número binario que resolveremos es: <br> 1110 "); //Document.write nos permite mostrar en pantalla el texto que pongamos entre los parenteisis "()" 
+numero = 1* Math.pow(2, 3) + 1* Math.pow(2, 2) + 1* Math.pow(2, 1) + 0* Math.pow(2, 0);   // le damos un valor a la variable numero con la operación correspondiente - Math.pow es una función que eleva a una potencia
+document.write("<br>El resultado es: "+numero +"<br><br>"); //Se muestra el resultado concatenando texto con la variable a mostrar
+document.write("<hr>"); // "hr" Es una etiquieta de html para crear una raya que separe las operaciones
+//=================================================================================================================================
+document.write("El número binario que resolveremos es: <br> 1111 "); //Document.write nos permite mostrar en pantalla el texto que pongamos entre los parenteisis "()" 
+numero = 1* Math.pow(2, 3) + 1* Math.pow(2, 2) + 1* Math.pow(2, 1) + 1* Math.pow(2, 0);   // le damos un valor a la variable numero con la operación correspondiente - Math.pow es una función que eleva a una potencia
+document.write("<br>El resultado es: "+numero +"<br><br>"); //Se muestra el resultado concatenando texto con la variable a mostrar
+document.write("<hr>"); // "hr" Es una etiquieta de html para crear una raya que separe las operaciones
+//=================================================================================================================================
+
+
+var Numero_x;
+Numero_x=prompt("Ingrese un numero");
+if (Numero_x==12) {
+
+	document.write("Su numero es 12");
+}
+else
+{
+	document.write("No es 12");
+}
